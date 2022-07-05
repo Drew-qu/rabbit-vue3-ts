dd0dc96d19f2229d5e74da636f55d8d1d4156c95
xiaoqu <1047910806@qq.com>
2022-07-05 16:28:54 +0800

    feat: 底部布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,9 +1,20 @@
 <script lang="ts" setup name="Layout">
 import AppTopnav from './component/app-topnav.vue';
+import AppHeader from './component/app-header.vue';
+import AppFooter from './component/app-footer.vue';
 
 </script>
 <template>
   <AppTopnav />
+  <AppHeader />
+  <main class="app-body">
+    <!-- 路由出口 -->
+  </main> 
+  <AppFooter />
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.app-body {
+  min-height: 600px;
+}
+</style>
