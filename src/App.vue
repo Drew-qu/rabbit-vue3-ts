26c7d60407e0fbf60174761d859d6bec2d104e19
xiaoqu <1047910806@qq.com>
2022-07-05 15:29:40 +0800

    feat: 配置路由模块

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div>
-    111  
+    <RouterView></RouterView>
   </div>
-  <h1>hi的话手动觉得v篇</h1>
 </template>
 
 <script setup lang="ts">
-
-import request from '@/utils/request'
-request.get('/home/index').then(res => console.log(res))
 </script>
 
 <style scoped lang="less">
 // less 引入语法: @import '文件路径'
-@import './assets/styles/variables.less';
-@import './assets/styles/mixins.less';
-h1 {
-  background-color: @xtxColor; // 使用引入文件内部变量 @变量名
-  .hoverShadow ()  // 使用引入文件内部定义方法
-}
+// @import './assets/styles/variables.less';
+// @import './assets/styles/mixins.less';
+// h1 {
+//   background-color: @xtxColor; // 使用引入文件内部变量 @变量名
+//   .hoverShadow ()  // 使用引入文件内部定义方法
+// }
 
 </style>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup name="Layout"></script>
+<template>
+  <div>layout组件</div>
+  <div>顶栏</div>
+  <div>导航</div>
+  <div>二级路由</div>
+  <div>下栏</div>
+</template>
+
+<style lang="less" scoped></style>
